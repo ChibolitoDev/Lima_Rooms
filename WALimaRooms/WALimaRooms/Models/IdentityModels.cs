2e84ae1940e597b51877ea2d04e325deb34bf900
ErnestoVC <netito86@hotmail.com>
2019-04-24 00:37:10 -0500

    ErnestoVC
    Ni IDEA solo por sí acaso

---
+++ WALimaRooms/WALimaRooms/Models/IdentityModels.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace WALimaRooms.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+    }
+}
---
+++ WALimaRooms/WALimaRooms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WALimaRooms.Startup))]
+namespace WALimaRooms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
